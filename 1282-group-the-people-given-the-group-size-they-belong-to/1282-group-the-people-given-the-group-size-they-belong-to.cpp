77238d5ff081b74ebf8029f038aab9f604152ff9
himanshusaini4589-lgtm <himanshusaini4589@gmail.com>
2026-07-21 22:37:43 +0530

    Time: 4 ms (51.04%), Space: 16.8 MB (60.33%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
+        unordered_map<int, vector<int>> mp;
+        vector<vector<int>> ans;
+
+        for (int i = 0; i < groupSizes.size(); i++) {
+            mp[groupSizes[i]].push_back(i);
+
+            if (mp[groupSizes[i]].size() == groupSizes[i]) {
+                ans.push_back(mp[groupSizes[i]]);
+                mp[groupSizes[i]].clear();
+            }
+        }
+
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
