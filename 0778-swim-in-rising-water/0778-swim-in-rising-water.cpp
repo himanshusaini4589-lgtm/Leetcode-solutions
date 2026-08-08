55899be58cff4e96c14401c4dbafa6cb9aec46cd
himanshusaini4589-lgtm <himanshusaini4589@gmail.com>
2026-08-08 20:44:54 +0530

    Time: 11 ms (39.32%), Space: 14 MB (48.15%) - LeetHub - Time: O(n^2 log n), Space: O(n^2)

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.cpp
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.cpp
@@ -2,27 +2,33 @@ class Solution {
 public:
     int swimInWater(vector<vector<int>>& grid) {
         int n = grid.size();
-vector<vector<int>> dist(n, vector<int>(n, INT_MAX));
-priority_queue<array<int,3>, vector<array<int,3>>, greater<>> pq;
-dist[0][0] = grid[0][0];
-pq.push({grid[0][0], 0, 0});
-int dr[] = {-1,1,0,0}, dc[] = {0,0,-1,1};
-while (!pq.empty()) {
-auto [d, r, c] = pq.top(); pq.pop();
-if (d > dist[r][c]) continue;
-if (r == n-1 && c == n-1) return d;
-for (int i = 0; i < 4; i++) {
-int nr = r+dr[i], nc = c+dc[i];
-if (nr>=0 && nr<n && nc>=0 && nc<n) {
-int nd = max(d, grid[nr][nc]);
-if (nd < dist[nr][nc]) {
-dist[nr][nc] = nd;
-pq.push({nd, nr, nc});
-}
-}
-}
-}
-return -1;
+        vector<vector<int>> dist(n, vector<int>(n, INT_MAX));
+
+        priority_queue<array<int,3>, vector<array<int,3>>, greater<>> pq;
+
+        dist[0][0] = grid[0][0];
+        pq.push({grid[0][0], 0, 0});
+
+        int dr[] = {-1,1,0,0}, dc[] = {0,0,-1,1};
+
+        while (!pq.empty()) {
+            auto [d, r, c] = pq.top(); pq.pop();
+
+            if (d > dist[r][c]) continue;
+            if (r == n-1 && c == n-1) return d;
+            
+            for (int i = 0; i < 4; i++) {
+                int nr = r+dr[i], nc = c+dc[i];
+                if (nr>=0 && nr<n && nc>=0 && nc<n) {
+                    int nd = max(d, grid[nr][nc]);
+                    if (nd < dist[nr][nc]) {
+                        dist[nr][nc] = nd;
+                        pq.push({nd, nr, nc});
+                    }
+                }
+            }
+        }
+        return -1;
     }
 };
 
