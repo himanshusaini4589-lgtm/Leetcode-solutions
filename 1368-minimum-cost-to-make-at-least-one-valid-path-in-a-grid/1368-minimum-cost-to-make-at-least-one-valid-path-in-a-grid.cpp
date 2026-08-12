7d5d0a1451d2b2611e46811b2bebec500f1804f6
himanshusaini4589-lgtm <himanshusaini4589@gmail.com>
2026-08-12 12:42:14 +0530

    Time: 99 ms (13.05%), Space: 30.2 MB (19.16%) - LeetHub - Time: O(nm log(nm)), Space: O(nm)

--- 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
+++ 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
@@ -12,12 +12,7 @@ public:
         };
 
         vector<vector<int>> dist(n, vector<int>(m, INT_MAX));
-
-        priority_queue<
-            vector<int>,
-            vector<vector<int>>,
-            greater<vector<int>>
-        > pq;
+        priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>> pq;
 
         dist[0][0] = 0;
         pq.push({0, 0, 0});   // {cost, row, col}
@@ -25,23 +20,17 @@ public:
         while(!pq.empty()) {
             auto curr = pq.top();
             pq.pop();
-
             int cost = curr[0];
             int r = curr[1];
             int c = curr[2];
-
-            // if(cost > dist[r][c])
-            //     continue;
-
-            // if(r == n - 1 && c == m - 1)
-            //     return cost;
+            if(cost > dist[r][c]) continue;
+            if(r == n - 1 && c == m - 1) return cost;
 
             for(int i = 0; i < 4; i++) {
                 int nr = r + dir[i].first;
                 int nc = c + dir[i].second;
 
-                if(nr < 0 || nr >= n || nc < 0 || nc >= m)
-                    continue;
+                if(nr < 0 || nr >= n || nc < 0 || nc >= m) continue;
 
                 int wt = (grid[r][c] == i + 1) ? 0 : 1;
 
