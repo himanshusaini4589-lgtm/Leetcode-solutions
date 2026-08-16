80d3e7fd48215c98561a63b7bf7e5b488edf7f1e
himanshusaini4589-lgtm <himanshusaini4589@gmail.com>
2026-08-16 10:55:54 +0530

    Time: 0 ms (100%), Space: 12.5 MB (97.69%) - LeetHub - Time: O(n^2), Space: O(n)

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -5,15 +5,14 @@ public:
         int n = triangle.size();
 
         vector<int>prev (n,0);
-        for(int i = 0 ; i< n ; i++){
+        for(int i = 0 ; i<n ; i++){
             prev[i] = triangle[n-1][i];
         }
-        vector<int>curr = prev;
+
         for(int i = n-2 ; i>=0 ; i--){
             for(int j = 0 ; j<=i ; j++){
-                curr[j] = triangle[i][j] + min(prev[j],prev[j+1]);
+                prev[j] = triangle[i][j] + min(prev[j], prev[j+1]);
             }
-            prev = curr;
         }
         return prev[0];
     }
