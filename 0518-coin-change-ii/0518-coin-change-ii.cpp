7300867569097adb636069c7c577dfd3919d6438
himanshusaini4589-lgtm <himanshusaini4589@gmail.com>
2026-08-16 20:58:48 +0530

    Time: 47 ms (24.49%), Space: 36.7 MB (39.86%) - LeetHub - Time: O(n * amount), Space: O(n * amount)

---
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int solve(int n, int amount, vector<int>& coins, vector<vector<int>>& dp) {
+        if (amount == 0)
+            return 1;
+        if (amount < 0 || n >= coins.size())
+            return 0;
+
+        if (dp[n][amount] != -1)
+            return dp[n][amount];
+
+        int take = solve(n, amount - coins[n], coins, dp);
+
+        int skip = solve(n+1, amount, coins, dp);
+
+        dp[n][amount] = take + skip;
+
+        return dp[n][amount];
+    }
+
+    int change(int amount, vector<int>& coins) {
+        vector < vector<int>> dp(coins.size(), vector<int>(amount + 1, -1));
+
+        return solve(0, amount, coins, dp);
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
