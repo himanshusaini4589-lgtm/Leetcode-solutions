f51673f383847b8f765007521a5a3c2659f6760d
himanshusaini4589-lgtm <himanshusaini4589@gmail.com>
2026-08-19 21:14:23 +0530

    Time: 406 ms (42.16%), Space: 211.6 MB (40.43%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,8 +1,6 @@
-
 int dp[100005][10][2];
 class Solution {
-public:
-    
+public:    
     int f(vector<int>&profit ,int  i , int k , bool on){
         if(i==profit.size()){
             return 0;
@@ -10,16 +8,14 @@ public:
         if(dp[i][k][on]!= -1) return dp[i][k][on];
         int result = 0;
 
-        if(on){
-            result = max(result,f(profit,i+1,k,false)-profit[i]);
+        if(on  && k>0){
+            result = max(result,f(profit,i+1,k-1,false)-profit[i]);
             result = max(result ,  f(profit,i+1,k,true));
         }
-
-        else if(!on && k>0){
-            result = max(result,f(profit,i+1,k-1,true) + profit[i]);
+        else if(!on){
+            result = max(result,f(profit,i+1,k,true) + profit[i]);
             result = max(result,f(profit,i+1,k,false));
         }
-
         return dp[i][k][on] = result;
     }
     
@@ -27,19 +23,14 @@ public:
         int n = prices.size();
 
         // dp[i][buy][cap]
-        vector<vector<vector<int>>> dp(
-            n + 1, vector<vector<int>>(2, vector<int>(3, 0))
-        );
-
+        vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2, vector<int>(3, 0)));
         // Build DP table backward
         for (int i = n - 1; i >= 0; i--) {
             for (int buy = 0; buy <= 1; buy++) {
                 for (int cap = 1; cap <= 2; cap++) {
-
                     if (buy == 1) {
                         // Can buy
-                        dp[i][buy][cap] = max(
-                            -prices[i] + dp[i + 1][0][cap], // buy today
+                        dp[i][buy][cap] = max(-prices[i] + dp[i + 1][0][cap], // buy today
                             dp[i + 1][1][cap]               // skip
                         );
                     } else {
