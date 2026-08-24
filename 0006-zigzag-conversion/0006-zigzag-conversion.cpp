f4f598e96ea6613b4f6dd84bf218a97184e0a1ab
himanshusaini4589-lgtm <himanshusaini4589@gmail.com>
2026-08-24 22:46:00 +0530

    Time: 4 ms (62.16%), Space: 13.9 MB (79.81%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        if (numRows == 1 || numRows >= s.size()) {
+            return s;
+        }
+
+        vector<string> rows(numRows);
+
+        int row = 0;
+        int direction = 1;
+
+        for (char ch : s) {
+            rows[row] += ch;
+
+            if (row == 0) {
+                direction = 1;
+            }
+            else if (row == numRows - 1) {
+                direction = -1;
+            }
+
+            row += direction;
+        }
+
+        string ans = "";
+
+        for (string &str : rows) {
+            ans += str;
+        }
+
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
