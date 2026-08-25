2272ab352710a4d6116e36b26d1d674691755483
himanshusaini4589-lgtm <himanshusaini4589@gmail.com>
2026-08-25 22:03:33 +0530

    Time: 530 ms (45.08%), Space: 15.3 MB (82.03%) - LeetHub

--- 1563-stone-game-v/1563-stone-game-v.cpp
+++ 1563-stone-game-v/1563-stone-game-v.cpp
@@ -1,48 +1,52 @@
 class Solution {
 public:
     int dp[501][501];
-    int f(vector<int>& nums, int start, int end, vector<int>& prefix) {
-        if (start >= end) return 0;
-        if(dp[start][end]!=-1) return dp[start][end];
-        int ans = 0;
-        for (int k = start; k < end; k++) {
-
-            int leftSum = prefix[k + 1] - prefix[start];
-            int rightSum = prefix[end + 1] - prefix[k + 1];
-
-            if (leftSum < rightSum) {
-                ans = max(ans,
-                    leftSum + f(nums, start, k, prefix)
-                );
-            }
-            else if (rightSum < leftSum) {
-                ans = max(ans,
-                    rightSum + f(nums, k + 1, end, prefix)
-                );
-            }
-            else {
-                ans = max(ans,
-                    leftSum + max(
-                        f(nums, start, k, prefix),
-                        f(nums, k + 1, end, prefix)
-                    )
-                );
-            }
-        }
-
-        return dp[start][end] = ans;
-    }
-
+    
     int stoneGameV(vector<int>& stoneValue) {
         int n = stoneValue.size();
-        memset(dp,-1,sizeof(dp));
+        memset(dp,0,sizeof(dp));
         vector<int> prefix(n + 1, 0);
 
         for (int i = 0; i < n; i++) {
             prefix[i + 1] = prefix[i] + stoneValue[i];
         }
 
-        return f(stoneValue, 0, n - 1, prefix);
+        for(int start = n-1 ; start>=0 ; start--){
+            for(int end = 0 ; end<n ; end++){
+                if(start>=end){
+                    dp[start][end] = 0;
+                    continue;
+                } 
+                int ans = 0;
+
+                for (int k = start; k < end; k++) {
+
+                    int leftSum = prefix[k + 1] - prefix[start];
+                    int rightSum = prefix[end + 1] - prefix[k + 1];
+
+                    if (leftSum < rightSum) {
+                        ans = max(ans,
+                            leftSum + dp[start][k]
+                        );
+                    }
+                    else if (rightSum < leftSum) {
+                        ans = max(ans,
+                            rightSum + dp[k+1][end]
+                        );
+                    }
+                    else {
+                        ans = max(ans,
+                            leftSum + max(
+                                dp[start][k],
+                                dp[k+1][end]
+                            )
+                        );
+                    }
+                }
+                dp[start][end] = ans;
+            }
+        }
+        return dp[0][n-1];
     }
 };
 
