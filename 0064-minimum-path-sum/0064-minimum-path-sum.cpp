cb88f3fbd625d2202541c43a1469759a455c04c4
himanshusaini4589-lgtm <himanshusaini4589@gmail.com>
2026-08-29 22:32:14 +0530

    Time: 0 ms (100%), Space: 16.8 MB (16.31%) - LeetHub - Time: O(m * n), Space: O(m * n)

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -2,25 +2,25 @@ class Solution {
 public:
     vector<vector<long long>> dp;
 
-    long long solve(vector<vector<int>>& grid, int m, int n) {
-        if (m < 0 || n < 0) return LLONG_MAX;
-        if (m == 0 && n == 0) return grid[0][0];
-
-        if (dp[m][n] != -1) return dp[m][n];
-
-        long long ans = grid[m][n] +
-            min(solve(grid, m-1, n), solve(grid, m, n-1));
-
-        return dp[m][n] = ans;
-    }
-
     int minPathSum(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
 
-        dp.assign(m, vector<long long>(n, -1));
+        dp.assign(m, vector<long long>(n, 0));
+        dp[0][0] = grid[0][0];
+        for(int i = 1 ; i< n ; i++){
+            dp[0][i] = dp[0][i-1] + grid[0][i];
+        }
+        for(int i = 1 ; i< m ; i++){
+            dp[i][0] = dp[i-1][0] + grid[i][0];
+        }
 
-        return solve(grid, m-1, n-1);
+        for(int i = 1 ; i< m ; i++ ){
+            for(int j = 1 ; j<n ; j++){
+                dp[i][j] = grid[i][j] + min(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
 
