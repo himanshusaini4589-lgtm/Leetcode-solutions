afbaec4d4af2c0c08d8aba0d34b563509a22c462
himanshusaini4589-lgtm <himanshusaini4589@gmail.com>
2026-08-31 21:42:14 +0530

    Time: 19 ms (78.88%), Space: 149.4 MB (10.5%) - LeetHub - Time: O(n*m), Space: O(n*m)

---
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.cpp
@@ -0,0 +1,29 @@
+class NumMatrix {
+public:
+    int n,m;
+    vector<vector<int> > prefix;
+    NumMatrix(vector<vector<int>>& matrix) {
+        n = matrix.size();
+        m = matrix[0].size();
+        prefix.resize(n+1,vector<int>(m+1,0));
+        for(int i = 0 ; i<n ; i++){
+            for(int j = 0 ; j<m ; j++){
+                prefix[i+1][j+1] = matrix[i][j] + prefix[i+1][j] + prefix[i][j+1] -prefix[i][j];
+            }
+        }
+    }
+    
+    int sumRegion(int row1, int col1, int row2, int col2) {
+        return prefix[row2+1][col2+1] - prefix[row2+1][col1] - prefix[row1][col2+1] + prefix[row1][col1];
+    }
+};
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * NumMatrix* obj = new NumMatrix(matrix);
+ * int param_1 = obj->sumRegion(row1,col1,row2,col2);
+ */
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
